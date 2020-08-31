32e6255c5adf2dfe7a334247ce84223b777a5024
Tim Dithmer <dith.tim@gmail.com>
2020-08-31 10:33:14 +0200

    vuejs 3 rc 9 template with ts, pug and scss

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+h1 Hallo Welt
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+const App = defineComponent({
+  name: 'App',
+})
+
+export default App
+</script>
+
+<style lang="scss"></style>
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-plain-loader',
+      },
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-typescript'],
+            },
+          },
+          { loader: 'ts-loader', options: { appendTsSuffixTo: [/\.vue$/] } },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html'),
+    }),
+  ],
+}
