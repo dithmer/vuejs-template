bc2ca6eaf3508a0ca13c5205ae4149d561dc2492
Tim Dithmer <dith.tim@gmail.com>
2020-09-26 11:40:54 +0200

    Finalize actualization of template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template lang="pug">
-h1 Hallo Welt
+div(class="h-full flex flex-col justify-center items-center")
+    h1(
+        class="text-2xl"
+    ) Vue.js 3 Template
+    p(class="w-full ml-12") Including
+    ul(class="w-full list-disc ml-24")
+        li Pug
+        li Typescript
+        li scss in Components
+        li Fontawesome
+        li Tailwind
 </template>
 
 <script lang="ts">
