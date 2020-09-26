8d12ae9dc0366fc86633125928551aba32d172d2
Tim Dithmer <dith.tim@gmail.com>
2020-09-26 11:44:46 +0200

    Nice response alignment

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template lang="pug">
-div(class="h-full flex flex-col justify-center items-center")
-    h1(
-        class="text-2xl"
-    ) Vue.js 3 Template
-    p(class="w-full ml-12") Including
-    ul(class="w-full list-disc ml-24")
-        li Pug
-        li Typescript
-        li scss in Components
-        li Fontawesome
-        li Tailwind
+div(class="h-full flex justify-center items-center ")
+    div(class="flex flex-col justify-center items-center md:w-64")
+        h1(
+            class="text-2xl"
+        ) Vue.js 3 Template
+        p(class="w-full ml-12") Including
+        ul(class="w-full list-disc ml-24")
+            li Pug
+            li Typescript
+            li scss in Components
+            li Fontawesome
+            li Tailwind
 </template>
 
 <script lang="ts">
