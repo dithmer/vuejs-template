713dc72b3454674e179c915986ef450d4e242fba
Tim Dithmer <dith.tim@gmail.com>
2020-10-01 09:43:59 +0200

    Add fontawesome std imports

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import App from './App.vue'
 import './tailwind.css'
 import './styles.scss'
 
+import '../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js'
+import '../node_modules/@fortawesome/fontawesome-free/js/brands.js'
+import '../node_modules/@fortawesome/fontawesome-free/js/regular.js'
+import '../node_modules/@fortawesome/fontawesome-free/js/solid.js'
+
 const app = createApp(App)
 
 app.mount('#app')
